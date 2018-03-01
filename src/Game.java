14b62c03c484a2883e21facdc716723533ac3323
Shim Young Lee <sylee1@stanford.edu>
2018-03-01 12:59:17 -0800

    minor method names updates, sets instead of lists for possessions and pages

--- src/Game.java
+++ src/Game.java
@@ -7,59 +7,79 @@ import java.util.*;
 
 public class Game {
 
-	private List<Page> pages;
-	private List<Shape> possessionList;
+	private String name;
 	private Page firstPage;
 	private Page currPage;
+	private Set<Page> pages;
+	private Set<Shape> possessions;
 
-	Game() {
-		this.pages = new ArrayList<Page>();
-		this.possessionList = new ArrayList<Shape>();
+	Game(String name) {
+		this.name = name;
+		this.pages = new HashSet<Page>();
+		this.possessions = new HashSet<Shape>();
 		this.firstPage = new Page("page1");
 		this.currPage  = firstPage;
 		this.pages.add(firstPage);
 	}
-	
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public void changeCurrPage(Page page) {
 		this.currPage = page;
 		//updateCanvas();
-		//currPage.onEnter() // call at end of function
+		//currPage.onEnter();
 	}
-	
-	
-	
+
 	public void addPage(Page page) {
 		this.pages.add(page);
 	}
-	
-	public void deletePage(Page page) {
-		for (int i = 0; i < this.pages.size(); i++) {
-			if (this.pages.get(i).equals(page)) {
-				this.pages.remove(i);
+
+	public void removePage(Page page) {
+		this.pages.remove(page);
+	}
+
+	public void removePage(String name) {
+		for (Page page : this.pages) { 
+			if (page.getName().equals(name)) {
+				this.pages.remove(page);
 				break;
 			}
 		}
 	}
-	
-	public void deletePage(String name) {
-		for (int i = 0; i < this.pages.size(); i++) {
-			if (this.pages.get(i).getName().equals(name)) {
-				this.pages.remove(i);
+
+	public void addPossession(Shape shape) {
+		this.possessions.add(shape);
+	}
+
+	public void removePossession(Shape shape) {
+		this.possessions.remove(shape);
+	}
+
+	public void removePossession(String name) {
+		for (Shape shape : this.possessions) { 
+			if (shape.getName().equals(name)) {
+				this.possessions.remove(shape);
 				break;
 			}
 		}
-	}	
+	}
 
+	public boolean equals(Game game) {
+		return this.name.equals(game.getName()); 
+	}
 
-	
 	// Random stuff for checking. Delete these
 	public static void main(String[] args) {
-		Game g = new Game();	
+		Game g = new Game("game1");	
 		BufferedImage img = null;
 		try {
 		    img = ImageIO.read(new File("raw/carrot.png"));
-		    
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -68,5 +88,4 @@ public class Game {
 		System.out.println(img.getWidth());
 		System.out.println(img.toString());
 	}
-	
 }
