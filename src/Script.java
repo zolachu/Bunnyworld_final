c7f05906e710c8dd278d097f5b737b977081c640
Nicholas Seay <nicholasseay@DN0a24a703.SUNet>
2018-03-02 13:17:38 -0800

    initial script design

--- src/Script.java
+++ src/Script.java
@@ -1,6 +1,47 @@
+import java.util.HashMap;
 
-public class Script {
+
+/**
+ * Script text is set of clauses where words are separated by spaces and clauses
+ * are separated by semicolon(;). Clause order is insignificant
+ * @author nicholasseay
+ *
+ */
+public  class Script {
+	//HashMap scriptInstructions<triggerword, trigger and Action String (raw input)>
 	
-	//good luck nick
-	// Nick: thanks	
+	//TRIGGERS
+	//onclick<action>
+	//onenter<actions>
+	//ondrop<shape name><actions>
+
+	//ACTIONS
+	//goto<page name>
+	//play< sound name>
+	//hide<shape name>
+	//show<shape name>
+
+	
+	static void parse(String allInstructions, HashMap<String, String> map) { // <trigger, instruction>
+		
+	}
+	
+	/* Given a clause, the Script decides what the action is and what its inputs
+	 * are, then performs the desired action
+	 */
+	static void perform(String instruction) {
+		
+	}
+	//reads the trigger
+
+	static void onClick() {	// wait do actions need to be separated like this?
+		
+	}
+	static void onEnter() {
+		
+	}
+	
+	static void onDrop() {
+		
+	}
 }
