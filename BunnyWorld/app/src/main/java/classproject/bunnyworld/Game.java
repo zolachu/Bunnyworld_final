0115f8325590ceb27ced6a2cd27782c4bc5aa58a
Nicholas Seay <nicholasseay@DN0a1f0b14.SUNet>
2018-03-04 14:17:21 -0800

    I think these were only changes to make things compile

--- BunnyWorld/app/src/main/java/classproject/bunnyworld/Game.java
+++ BunnyWorld/app/src/main/java/classproject/bunnyworld/Game.java
@@ -3,8 +3,10 @@ package classproject.bunnyworld;
 //import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.lang.String;
 
 //import javax.imageio.ImageIO;
 
@@ -15,6 +17,119 @@ public class Game {
 	private GPage currPage;
 	private Set<GPage> pages;
 	private Set<GShape> possessions;
+
+
+
+	/**
+	 * Script text is set of clauses where words are separated by spaces and clauses
+	 * are separated by semicolon(;). Clause order is insignificant
+	 *
+	 * TRIGGERS
+	 * onclick<action>
+	 * onenter<actions>
+	 * ondrop<shape name><actions>
+	 *
+	 * ACTIONS
+	 * goto<page_name>
+	 * play< sound_name>
+	 * hide<shape_name>
+	 * show<shape_name>
+	 * @author nicholasseay
+	 *
+	 */
+	public  class Script {
+
+		/* Populates the given map using the string of raw script text
+         * keys will be one of the Triggers above, and the value will be the complete
+         * list of instructions to be completed in the event of the trigger.
+         */
+		void parse(String scriptText, HashMap<String, String> map) { // <trigger, instruction>
+			String[] allInstructions = scriptText.split(";");
+
+			for(String instruct: allInstructions) {
+				int index = instruct.indexOf(' ');
+				String trigger = instruct.substring(0, index);
+				String action = instruct.substring(index + 1); // eliminate space at the end
+				map.put(trigger, action);
+			}
+		}
+
+		/* Given a clause, the Script decides what the action is and what its inputs
+         * are, then performs the desired action
+         */
+		void perform(String instruct) {
+			if(instruct == null) return;
+
+			int index = instruct.indexOf(' ');
+			String action = instruct.substring(0, index);
+			String param = instruct.substring(index + 1); // eliminate space at the end
+
+			switch (action) {
+				case "goTo":
+					goTo(param);
+					break;
+				case "play":
+					play(param);
+					break;
+				case "hide":
+					hideOrShow(param, true);
+					//updateCanvas();
+					break;
+				case "show":
+					hideOrShow(param, false);
+					//updateCanvas();
+					break;
+			}
+		}
+
+		//reads the trigger
+
+		/* switches game's current page
+         * this function assumes only one page
+         */
+		void goTo(String param) {
+			GPage page = getPage(param);
+			if(page != null) changeCurrPage(page);
+		}
+
+		/* plays the sounds given by param
+         * assumes there are no spaces within file names
+         */
+		void play(String param) {
+
+			// not yet complete
+
+		}
+
+		/* hides or shows the shapes given by param
+         * based on the boolean passed into hide.
+         * If hide is true, shape will be hidden.
+         * (switches their hidden boolean to true)
+         * assumes shape names contain no spaces
+         */
+		void hideOrShow(String param, boolean hide) {
+			String[] shapesToHide = param.split(" ");
+			for(String shapeName: shapesToHide) {
+				GShape curr = findShape(shapeName);
+				if(curr != null) curr.setHidden(hide);
+			}
+		}
+
+		/* searches pages for and returns a GShape by the name
+         * of shapeID or null if none exists
+         */
+		GShape findShape(String shapeID) {
+			for(GPage page: pages) {
+				GShape shape = page.getShape(shapeID);
+				if(shape != null) return shape;
+			}
+			return null;
+		}
+
+
+	}
+
+
 
 	Game(String name) {
 		this.name = name;
