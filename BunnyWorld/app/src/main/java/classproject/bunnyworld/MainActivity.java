2189d4d92df2dfbd9e7a3edfdeb953a85a391dad
zolboozzzzzzz <36867950+zolboozzzzzzz@users.noreply.github.com>
2018-03-05 23:38:15 -0800

    Update MainActivity.java

--- BunnyWorld/app/src/main/java/classproject/bunnyworld/MainActivity.java
+++ BunnyWorld/app/src/main/java/classproject/bunnyworld/MainActivity.java
@@ -4,16 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.database.sqlite.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = openOrCreateDatabase("games.db", MODE_PRIVATE, null);
 
-        Game game = new Game("g1");
-        System.err.println(game);
+        
+        // Table for Shapes
+        // each shape belongs to either one of Possession or a particular Page.
+
+        Cursor shapesCursor = db.rawQuery("SELECT * FROM sqlite_master "
+                + "WHERE type='table' AND name ='shapes';" , null);
+
+        if (shapesCursor.getCount() == 0) {
+            String setupStr = "CREATE TABLE shapes ("
+                    + "gameName TEXT, page TEXT, shape TEXT, script TEXT,  x INTEGER, y INTEGER, w INTEGER, h INTEGER"
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT"
+                    + ");";
+            db.execSQL(setupStr);
+
+            String dataStr= "INSERT INTO pages VALUES "
+                    + "('Game1','Page1','',,,,, NULL);";
+
+            db.execSQL(dataStr);
+        }
+        
+//         Game game = new Game("g1");
+//         System.err.println(game);
     }
 
     public void onNewGame(View view) {
