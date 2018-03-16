dcf9fcca49b2cfff6e2a09d6a7a1b0390a789147
Nicholas Seay <nicholasseay@DN0a22c631.SUNet>
2018-03-15 17:36:37 -0700

    implemented Script Activity error checking/forced correct input

--- BunnyWorld/app/src/main/java/classproject/bunnyworld/ScriptActivity.java
+++ BunnyWorld/app/src/main/java/classproject/bunnyworld/ScriptActivity.java
@@ -3,12 +3,10 @@ package classproject.bunnyworld;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.PopupMenu;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -50,6 +48,10 @@ public class ScriptActivity extends AppCompatActivity implements AdapterView.OnI
         spinnerSetUp3();
         spinnerSetUp4();
         spinnerSetUp5();
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Shapes may have 1 of any Trigger. Duplicates will overwrite the last.",
+                Toast.LENGTH_LONG);
+        toast.show();
 
     }
 
@@ -148,47 +150,158 @@ public class ScriptActivity extends AppCompatActivity implements AdapterView.OnI
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
         String item = parent.getItemAtPosition(pos).toString();
-        curScript.append(item + " ");
-        displayScript();
+        if(id > 0) {
+            parent.setVisibility(View.INVISIBLE);
+            thirdColumnStuffOn(view, false);
+            secondColumnStuffOn(view, true);
+            curScript.append(item + " ");
+            displayScript();
+        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
 
+    public void initializeButtonClickability(View view) {
+        // first column (triggers) made clickable
+        firstColumnStuffOn(view, true);
+
+        // second column (actions) made unclickable
+        secondColumnStuffOn(view, false);
+
+        // third column (targets) made unclickable
+        thirdColumnStuffOn(view, false);
+
+        // End Instruction Button
+        endClauseOn(view, false);
+    }
+
+    public void firstColumnStuffOn(View view, Boolean on) {
+        // first column (triggers)
+        Button onclick = (Button) findViewById(R.id.script_on_click_button);
+        onclick.setEnabled(on);
+        Button onenter = (Button) findViewById(R.id.script_on_enter_button);
+        onenter.setEnabled(on);
+        Button ondrop = (Button) findViewById(R.id.script_on_drop_button);
+        ondrop.setEnabled(on);
+        Spinner sender = (Spinner) findViewById(R.id.script_onDropTarget_spinner);
+        sender.setVisibility(View.INVISIBLE);
+    }
+
+    public void firstColumnButtonsOn(View view, Boolean on) {
+        // first column (triggers)
+        Button onclick = (Button) findViewById(R.id.script_on_click_button);
+        onclick.setEnabled(on);
+        Button onenter = (Button) findViewById(R.id.script_on_enter_button);
+        onenter.setEnabled(on);
+        Button ondrop = (Button) findViewById(R.id.script_on_drop_button);
+        ondrop.setEnabled(on);
+    }
+
+    public void secondColumnStuffOn(View view, Boolean on) {
+        // second column (actions)
+        Button go_to = (Button) findViewById(R.id.script_goto_button);
+        go_to.setEnabled(on);
+        Button play = (Button) findViewById(R.id.script_play_button);
+        play.setEnabled(on);
+        Button hide = (Button) findViewById(R.id.script_hide_button);
+        hide.setEnabled(on);
+        Button show = (Button) findViewById(R.id.script_show_button);
+        show.setEnabled(on);
+    }
+
+    public void thirdColumnStuffOn(View view, Boolean on) {
+        // third column (targets)
+        int visibility;
+        if(on) {
+            visibility = View.VISIBLE;
+        } else {
+            visibility = View.INVISIBLE;
+        }
+
+        Spinner gopage = (Spinner) findViewById(R.id.script_goto_spinner);
+        gopage.setVisibility(visibility);
+        Spinner sound = (Spinner) findViewById(R.id.script_play_spinner);
+        sound.setVisibility(visibility);
+        Spinner hidden = (Spinner) findViewById(R.id.script_hide_spinner);
+        hidden.setVisibility(visibility);
+        Spinner shown = (Spinner) findViewById(R.id.script_show_spinner);
+        shown.setVisibility(visibility);
+        if(on) {
+            gopage.setSelection(0);
+            sound.setSelection(0);
+            hidden.setSelection(0);
+            shown.setSelection(0);
+        }
+    }
+
+    public void endClauseOn(View view, Boolean on) {
+        // first column (triggers)
+        Button end = (Button) findViewById(R.id.script_semicolon_button);
+        end.setEnabled(on);
+    }
+
     public void onClickTrigger(View view) {
         curScript.append(ON_CLICK + " ");
         displayScript();
+        firstColumnStuffOn(view, false);
+        secondColumnStuffOn(view, true);
     }
 
     public void onEnterTrigger(View view) {
         curScript.append(ON_ENTER + " ");
         displayScript();
+        firstColumnStuffOn(view, false);
+        secondColumnStuffOn(view, true);
     }
 
     public void onDropTrigger(View view) {
         curScript.append(ON_DROP + " ");
         displayScript();
+        Spinner senderSpinner = (Spinner) findViewById(R.id.script_onDropTarget_spinner);
+        senderSpinner.setVisibility(View.VISIBLE);
+        firstColumnButtonsOn(view, false);
     }
 
     public void goToAction(View view) {
         curScript.append(GO_TO + " ");
         displayScript();
+        secondColumnStuffOn(view, false);
+        Spinner gopage = (Spinner) findViewById(R.id.script_goto_spinner);
+        gopage.setVisibility(View.VISIBLE);
+        gopage.setSelection(0);
+        endClauseOn(view, true);
     }
 
     public void playAction(View view) {
         curScript.append(PLAY + " ");
         displayScript();
+        secondColumnStuffOn(view, false);
+        Spinner sound = (Spinner) findViewById(R.id.script_play_spinner);
+        sound.setVisibility(View.VISIBLE);
+        sound.setSelection(0);
+        endClauseOn(view, true);
     }
 
     public void hideAction(View view) {
         curScript.append(HIDE + " ");
         displayScript();
+        secondColumnStuffOn(view, false);
+        Spinner hidden = (Spinner) findViewById(R.id.script_hide_spinner);
+        hidden.setVisibility(View.VISIBLE);
+        hidden.setSelection(0);
+        endClauseOn(view, true);
     }
 
     public void showAction(View view) {
         curScript.append(SHOW + " ");
         displayScript();
+        secondColumnStuffOn(view, false);
+        Spinner shown = (Spinner) findViewById(R.id.script_show_spinner);
+        shown.setVisibility(View.VISIBLE);
+        shown.setSelection(0);
+        endClauseOn(view, true);
     }
 
     private void displayScript() {
@@ -199,6 +312,22 @@ public class ScriptActivity extends AppCompatActivity implements AdapterView.OnI
     public void resetScript(View view) {
         curScript = new StringBuilder();
         displayScript();
+        initializeButtonClickability(view);
+    }
+
+    public void deleteLastClause(View view) {
+        String rightNow = curScript.toString();
+        String[] clauses = rightNow.split(";");
+        StringBuilder withoutLastClause = new StringBuilder();
+        int len = clauses.length;
+        for(int i = 0; i < len - 2; i++) { // minus 2 due to trailing space
+            withoutLastClause.append(clauses[i].trim());
+            withoutLastClause.append("; ");
+        }
+
+        curScript = withoutLastClause;
+        displayScript();
+        initializeButtonClickability(view);
     }
 
     public void addSemicolon(View view) {
@@ -206,12 +335,22 @@ public class ScriptActivity extends AppCompatActivity implements AdapterView.OnI
         curScript.deleteCharAt(len-1);
         curScript.append("; ");
         displayScript();
+        initializeButtonClickability(view);
     }
 
     public void saveScript(View view) {
+        if(curScript.charAt(curScript.length() - 2) != ';') {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Please complete current instruction or delete it.",
+                    Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
         GameManager gameManager = GameManager.getInstance();
         gameManager.setCurScript(curScript.toString().trim());
         curShape.setScriptText(curScript.toString().trim());
+        initializeButtonClickability(view);
 
 
         Intent intent = new Intent(this, EditorActivity.class);
